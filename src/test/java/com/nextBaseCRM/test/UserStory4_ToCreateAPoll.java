7f905be893d3f8e60ad31fa9cc614904b4c5a9a8
Murat Guney <muratguney@msn.com>
2021-01-05 23:28:12 -0500

    add web driver and login & password click

--- src/test/java/com/nextBaseCRM/test/UserStory4_ToCreateAPoll.java
+++ src/test/java/com/nextBaseCRM/test/UserStory4_ToCreateAPoll.java
@@ -1,11 +1,30 @@
 package com.nextBaseCRM.test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UserStory4_ToCreateAPoll {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("New Project");
+
+           WebDriverManager.chromedriver().setup();
+
+           WebDriver driver = new ChromeDriver();
+
+           driver.get("https://login2.nextbasecrm.com/stream/");
+
+           driver.manage().window().maximize();
+
+           driver.findElement(By.name("USER_LOGIN")).sendKeys("helpdesk39@cybertekschool.com");
+           driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser");
+           driver.findElement(By.className("login-btn")).click();
+
+
+
+
+
 
     }
 }
