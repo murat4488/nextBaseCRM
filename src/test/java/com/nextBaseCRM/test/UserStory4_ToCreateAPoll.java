0cc41b5112ef9a170c42586b8c1eed62429f9512
Murat Guney <muratguney@msn.com>
2021-01-05 22:37:04 -0500

    New Project

---
+++ src/test/java/com/nextBaseCRM/test/UserStory4_ToCreateAPoll.java
@@ -0,0 +1,11 @@
+package com.nextBaseCRM.test;
+
+import org.openqa.selenium.WebDriver;
+
+public class UserStory4_ToCreateAPoll {
+    public static void main(String[] args) {
+
+        System.out.println("New Project");
+
+    }
+}
